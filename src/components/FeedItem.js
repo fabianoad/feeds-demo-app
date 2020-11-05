97e45686b3d3b34cf0b3bbea0697253e90120ec7
fabianoad <fabianoad@yahoo.com.br>
2020-11-05 15:33:31 -0300

    initial commit

---
+++ src/components/FeedItem.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class FeedItem extends React.Component {
+    render() {
+        return <React.Fragment>
+                    <p>Feed</p>
+                <div className="feed-div"></div>
+        </React.Fragment>
+    }
+}
+export default FeedItem
